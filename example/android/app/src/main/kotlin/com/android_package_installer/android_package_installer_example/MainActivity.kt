b783820d72cdbb905c63419ec2ffbd0007f921c4
jinwook song <wlsdnr129@naver.com>
2025-03-31 17:12:52 +0900

    fix: error

--- example/android/app/src/main/kotlin/com/android_package_installer/android_package_installer_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.android_package_installer_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/src/installer_platform.dart
+++ lib/src/installer_platform.dart
@@ -1,18 +1,18 @@
-import 'package:android_package_installer/src/method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+import 'method_channel.dart';
 
 abstract class AndroidPackageInstallerPlatform extends PlatformInterface {
   AndroidPackageInstallerPlatform() : super(token: _token);
 
   static final Object _token = Object();
 
-  static AndroidPackageInstallerPlatform _instance =
-      MethodChannelAndroidPackageInstaller();
+  static AndroidPackageInstallerPlatform _instance = MethodChannelAndroidPackageInstaller();
 
   static AndroidPackageInstallerPlatform get instance => _instance;
 
   static set instance(AndroidPackageInstallerPlatform instance) {
-    PlatformInterface.verifyToken(instance, _token);
+    PlatformInterface.verify(instance, _token);
     _instance = instance;
   }
 
